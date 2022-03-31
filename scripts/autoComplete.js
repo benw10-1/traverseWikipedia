96410aa878ce4fd2361be67cb309f0592d4c982a
Ben Wirth <benjaminwirth10@gmail.com>
2022-03-30 23:15:23 -0700

    fix

--- scripts/autoComplete.js
+++ scripts/autoComplete.js
@@ -1,5 +1,5 @@
 function autocomplete(text) {
-    const q = "https://en.wikipedia.org/w/api.php?action=opensearch&limit=10&format=json&&origin=*search=a" + text
+    const q = `https://en.wikipedia.org/w/api.php?action=opensearch&limit=10&format=json&search=${text}&origin=*`
     
     return fetch(q).then(res => res.json()).then(data => {
         console.log(data)
