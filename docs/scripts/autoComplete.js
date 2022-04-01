b7650687d6276c1ca07b722cf39b67ad93e67256
Ben Wirth <benjaminwirth10@gmail.com>
2022-04-01 02:52:09 -0700

    comments and fixes

--- docs/scripts/autoComplete.js
+++
@@ -1,5 +0,0 @@
-function autocomplete(text) {
-    const q = `https://en.wikipedia.org/w/api.php?action=opensearch&limit=10&format=json&search=${text}&origin=*`
-    
-    return fetch(q).then(res => res.json())
-}
